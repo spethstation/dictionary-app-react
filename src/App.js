9669d8d12f0ccd13d754cc90d7d2d551da9933b9
spethstation <114708141+spethstation@users.noreply.github.com>
2023-03-15 18:03:41 -0300

    Initial commit, added header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.jpeg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Dictionary
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
